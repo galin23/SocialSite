8891a02b7f1b6d79147484fbf839af03fe947bc8
Galin Jelev <galin_pro@yahoo.com>
2020-04-02 22:08:01 +0300

    Visual update

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -20,7 +20,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
     path('admin/', admin.site.urls),
-    path('test/', views.TestPage.as_view(), name="test"),
     path('thanks/', views.ThanksPage.as_view(), name="thanks"),
     path('accounts/', include("accounts.urls", namespace="accounts")),
     path('accounts/', include("django.contrib.auth.urls")),
