456a29758ac0af02e4e4639b6da290a53640d0c8
Galin Jelev <galin_pro@yahoo.com>
2020-04-07 21:52:55 +0300

    Mobile view fix

--- posts/forms.py
+++ posts/forms.py
@@ -10,7 +10,7 @@ class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
-        if user is not None:
+        if user in group.members.all:
             self.fields["group"].queryset = (
                 models.Group.objects.filter(
                     pk__in=user.groups.values_list("group__pk")
